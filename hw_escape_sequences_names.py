ab715a9a505c15687d40bfa38fdbb0ec3805b26a
NishanthMekala <104415101+NishanthMekala@users.noreply.github.com>
2022-04-27 19:47:52 +0530

    Add files via upload

---
+++ hw_escape_sequences_names.py
@@ -0,0 +1,6 @@
+print('hello\soorya')#hello\soorya
+print('hello\harshan')#hello\harshan
+print('hello\abhishek')#hellobhishek
+print('hello\nishanth')#hello
+                       #ishanth
+print('hello\ramya')   #amyao #\r for reverse                    
